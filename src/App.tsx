8b31b2e0eac5e7859955e151b3f9b7a32d173807
Xenor <shokot_2005@mail.ru>
2023-04-19 22:35:59 +0300

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,32 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
+import { useEffect, useState } from 'react'
 import './App.css'
 import whiteRook from './assets/pieces/white_rook.png'
+import BoardComponent from './components/BoardComponent'
+import { Board } from './models/Board';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [board, setBoard] = useState(new Board());
+
+  useEffect(() => {
+    restart()
+  }, [])
+
+  function restart() {
+    const newBoard = new Board();
+    newBoard.initCells()
+    setBoard(newBoard)
+  }
+
+  interface BoardProps {
+    board: Board;
+    setBoard: (board: Board) => void;
+  }
+
+  
 
   return (
     <div className="App">
-        <a href="https://vitejs.dev" target="_blank">
-          <img width={50} src={whiteRook}/>
-        </a>
+      <BoardComponent />
     </div>
   )
 }
---
+++ src/components/BoardComponent.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function BoardComponent() {
+  return (
+    <div className='board'>
+        <div className="cell white"></div>
+        <div className="cell black"></div>
+        <div className="cell white"></div>
+        <div className="cell black"></div>
+    </div>
+  )
+}
